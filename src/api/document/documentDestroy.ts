44f419d3958a2125b49c438e3d09ed68e0717ca7
luckyman3559 <dybbuk555@gmail.com>
2022-12-24 00:37:27 +0900

    feat:created document endpoints and completed document back-end

---
+++ src/api/document/documentDestroy.ts
@@ -0,0 +1,22 @@
+import PermissionChecker from '../../services/user/permissionChecker';
+import ApiResponseHandler from '../apiResponseHandler';
+import Permissions from '../../security/permissions';
+import DocumentService from '../../services/documentService';
+
+export default async (req, res, next) => {
+  try {
+    new PermissionChecker(req).validateHas(
+      Permissions.values.documentDestroy,
+    );
+
+    await new DocumentService(req).destroyAll(
+      req.query.ids,
+    );
+
+    const payload = true;
+
+    await ApiResponseHandler.success(req, res, payload);
+  } catch (error) {
+    await ApiResponseHandler.error(req, res, error);
+  }
+};
---
+++ src/api/document/index.ts
@@ -0,0 +1,30 @@
+export default (app) => {
+  app.post(
+    `/tenant/:tenantId/document`,
+    require('./documentCreate').default,
+  );
+  app.put(
+    `/tenant/:tenantId/document/:id`,
+    require('./documentUpdate').default,
+  );
+  app.post(
+    `/tenant/:tenantId/document/import`,
+    require('./documentImport').default,
+  );
+  app.delete(
+    `/tenant/:tenantId/document`,
+    require('./documentDestroy').default,
+  );
+  app.get(
+    `/tenant/:tenantId/document/autocomplete`,
+    require('./documentAutocomplete').default,
+  );
+  app.get(
+    `/tenant/:tenantId/document`,
+    require('./documentList').default,
+  );
+  app.get(
+    `/tenant/:tenantId/document/:id`,
+    require('./documentFind').default,
+  );
+};
