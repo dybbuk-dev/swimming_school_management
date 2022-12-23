9192443e106bfc9ab2291a42dd20fb733e28a810
luckyman3559 <dybbuk555@gmail.com>
2022-12-24 00:20:09 +0900

    feat:created document service

---
+++ src/services/documentService.ts
@@ -0,0 +1,156 @@
+import { IServiceOptions } from './IServiceOptions';
+import Error400 from '../errors/Error400';
+import FileRepository from '../database/repositories/fileRepository';
+import MongooseRepository from '../database/repositories/mongooseRepository';
+import DocumentRepository from '../database/repositories/documentRepository';
+
+export default class DocumentService {
+  options: IServiceOptions;
+
+  constructor(options) {
+    this.options = options;
+  }
+
+  async create(data) {
+    const session = await MongooseRepository.createSession(
+      this.options.database,
+    );
+
+    try {
+      data.attachment =
+        await FileRepository.filterIdsInTenant(
+          data.attachment,
+          { ...this.options, session },
+        );
+
+      const record = await DocumentRepository.create(data, {
+        ...this.options,
+        session,
+      });
+
+      await MongooseRepository.commitTransaction(session);
+
+      return record;
+    } catch (error) {
+      await MongooseRepository.abortTransaction(session);
+
+      MongooseRepository.handleUniqueFieldError(
+        error,
+        this.options.language,
+        'document',
+      );
+
+      throw error;
+    }
+  }
+
+  async update(id, data) {
+    const session = await MongooseRepository.createSession(
+      this.options.database,
+    );
+
+    try {
+      data.attachment =
+        await FileRepository.filterIdsInTenant(
+          data.attachment,
+          { ...this.options, session },
+        );
+
+      const record = await DocumentRepository.update(
+        id,
+        data,
+        {
+          ...this.options,
+          session,
+        },
+      );
+
+      await MongooseRepository.commitTransaction(session);
+
+      return record;
+    } catch (error) {
+      await MongooseRepository.abortTransaction(session);
+
+      MongooseRepository.handleUniqueFieldError(
+        error,
+        this.options.language,
+        'document',
+      );
+
+      throw error;
+    }
+  }
+
+  async destroyAll(ids) {
+    const session = await MongooseRepository.createSession(
+      this.options.database,
+    );
+
+    try {
+      for (const id of ids) {
+        await DocumentRepository.destroy(id, {
+          ...this.options,
+          session,
+        });
+      }
+
+      await MongooseRepository.commitTransaction(session);
+    } catch (error) {
+      await MongooseRepository.abortTransaction(session);
+      throw error;
+    }
+  }
+
+  async findById(id) {
+    return DocumentRepository.findById(id, this.options);
+  }
+
+  async findAllAutocomplete(search, limit) {
+    return DocumentRepository.findAllAutocomplete(
+      search,
+      limit,
+      this.options,
+    );
+  }
+
+  async findAndCountAll(args) {
+    return DocumentRepository.findAndCountAll(
+      args,
+      this.options,
+    );
+  }
+
+  async import(data, importHash) {
+    if (!importHash) {
+      throw new Error400(
+        this.options.language,
+        'importer.errors.importHashRequired',
+      );
+    }
+
+    if (await this._isImportHashExistent(importHash)) {
+      throw new Error400(
+        this.options.language,
+        'importer.errors.importHashExistent',
+      );
+    }
+
+    const dataToCreate = {
+      ...data,
+      importHash,
+    };
+
+    return this.create(dataToCreate);
+  }
+
+  async _isImportHashExistent(importHash) {
+    const count = await DocumentRepository.count(
+      {
+        importHash,
+      },
+      this.options,
+    );
+
+    return count > 0;
+  }
+}
